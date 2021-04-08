fc44762f0fc3883b782434dbfdd33306273114b1
nicklas-holmqvist <nicklas_holmqvist@outlook.com>
2021-04-08 13:31:54 +0200

    created todos

--- server.js
+++ server.js
@@ -2,13 +2,40 @@ const express = require('express');
 const app = express()
 const port = 3000
 
+const todos = [
+    {
+        "id": 1,
+        "title": "städa"
+    },
+    {
+        "id": 2,
+        "title": "handla"
+    },
+    {
+        "id": 3,
+        "title": "sola"
+    }
+]
+
 app.use(express.json())
 
+app.get("/api", (req, res) => {
+    res.json('Hej, välkommen!')
+})
 
+app.get("/api/:id", (req, res) => {
+    const id = req.params.id
+    res.json("GET: " + id)
+})
+
+app.post("/api", (req, res) => {
+    const body = req.body.test
+    res.json(body)
+})
 
 
 
 app.listen(port, (req, res) => {
-    console.log('servern är igång')
+    console.log('servern är igång på: ' + port)
 })
 
