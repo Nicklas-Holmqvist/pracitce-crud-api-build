ff0fc2eab306e6a9ae085e5bfa17ff422c9ce6ee
nicklas-holmqvist <nicklas_holmqvist@outlook.com>
2021-04-08 13:16:55 +0200

    server är igång

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 const express = require('express');
 const app = express()
+const port = 3000
+
+app.use(express.json())
+
+
+
+
+
+app.listen(port, (req, res) => {
+    console.log('servern är igång')
+})
 
